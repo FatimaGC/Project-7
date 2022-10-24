0ff47ca01f1a6a061234d438e2e7775c5f879a8b
Fatima Guzman <fguzman1230@gmail.com>
2022-10-24 08:26:59 -0600

    Added daily traffic chart

--- web_app_dashboard_v3.2/app.js
+++ web_app_dashboard_v3.2/app.js
@@ -1,7 +1,7 @@
 // Traffic Chart
 // const traffic = document.getElementById("traffic-chart");
 
-const labels = [
+const trafficLabels = [
   "16-22",
   "23-29",
   "30-5",
@@ -15,8 +15,8 @@ const labels = [
   "25-31",
 ];
 
-const data = {
-  labels: labels,
+const trafficData = {
+  labels: trafficLabels,
   datasets: [
     {
       data: [550, 1400, 1000, 2000, 1500, 1700, 1200, 1800, 2300, 1500, 2500],
@@ -26,9 +26,36 @@ const data = {
   ],
 };
 
-const config = {
+const trafficConfig = {
   type: "line",
-  data: data,
+  data: trafficData,
 };
 
-const myChart = new Chart(document.getElementById("traffic-chart"), config);
+const trafficChart = new Chart(
+  document.getElementById("traffic-chart"),
+  trafficConfig
+);
+
+// DAILY TRAFFIC CHART
+const dailyTrafficLabels = ["S", "M", "T", "W", "T", "F", "S"];
+
+const dailyTrafficData = {
+  labels: dailyTrafficLabels,
+  datasets: [
+    {
+      data: [55, 120, 155, 130, 230, 200, 100],
+      fill: true,
+      borderColor: "rgb(75, 192, 192)",
+    },
+  ],
+};
+
+const dailyTrafficConfig = {
+  type: "bar",
+  data: dailyTrafficData,
+};
+
+const dailyTrafficChart = new Chart(
+  document.getElementById("daily-traffic-chart"),
+  dailyTrafficConfig
+);
